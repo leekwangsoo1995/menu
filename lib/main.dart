0e19667f16b5d70106e5f2ef511ff13d1b4c2261
Leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-05-24 19:41:08 +0900

    一覧リストの追加

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,48 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child: Text('リスト一覧画面'),
+      appBar: AppBar(
+        title: Text("リスト一覧画面"),
       ),
+      body:ListView(
+          children: <Widget>[
+
+            Text("hello"),
+            Text("hello2"),
+            Text("hello3"),
+          ],
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NextPage(),
+              )
+          );
+          // Add your onPressed code here!
+        },
+        child: const Icon(Icons.navigation),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("次のページ"),
+      ),
+      body:Center(
+        child:TextButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child:Text("戻ります"),
+        ),
+      )
     );
   }
 }
